91dd1b6ef6ba9e9c43f1bd89224513c485c58c28
Toniithebreadguy <154719320+Toniithebreadguy@users.noreply.github.com>
2024-08-13 11:47:13 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("\n\nHello and welcome!");
+        System.out.printf("\n\nHow are you today?");
+        System.out.printf("\n\nThis is how it works");
+
+}
+}
